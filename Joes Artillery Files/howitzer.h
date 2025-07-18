e57535845248e6289afe330309cf3e19fd58b67b
guynumeratorone <156556003+guynumeratorone@users.noreply.github.com>
2025-07-18 16:45:24 -0600

    Update howitzer.h

--- Joes Artillery Files/howitzer.h
+++ Joes Artillery Files/howitzer.h
@@ -13,7 +13,7 @@
 #include "angle.h"
 #include "position.h"
 #include "velocity.h"
-#include "physics.h"
+ // #include "physics.h"
 #include "uiDraw.h"
 
 
@@ -24,67 +24,64 @@
 
 class TestHowitzer;
 
- /*********************************************
-  * Howitzer
-  * Everything we need to know about a howitzer (aka the gun)
-  *********************************************/
+/*********************************************
+ * Howitzer
+ * Everything we need to know about a howitzer (aka the gun)
+ *********************************************/
 class Howitzer
 {
-   public:
-      // for unit tests
-      friend ::TestHowitzer;
+public:
+   // for unit tests
+   friend ::TestHowitzer;
 
-      // default constructor
-      Howitzer() : muzzleVelocity(DEFAULT_MUZZLE_VELOCITY), elevation(45.0) {}
+   // default constructor
+   Howitzer() : muzzleVelocity(DEFAULT_MUZZLE_VELOCITY), elevation(45.0) {}
+   // meter position constructor
+   Howitzer(double x, double y) {
+      position.setMetersX(x);
+      position.setMetersY(y);
+   }
 
-      Howitzer(double x, double y) : muzzleVelocity(DEFAULT_MUZZLE_VELOCITY), elevation(45.0)
-      {
-         position.setMetersX(x);
-         position.setMetersY(y);
-      }
+   // draw
+   void draw(ogstream& gout, double flightTime) const
+   {
+      gout.drawHowitzer(position,
+         elevation.getRadians(),
+         flightTime);
+   }
 
-      // draw
-      void draw(ogstream& gout, double flightTime) const
-      {
-         gout.drawHowitzer(position,
-            elevation.getRadians(),
-            flightTime);
-      }
+   // where is the howitzer at right now?
+   Position& getPosition() { return position; }
 
-      // where is the howitzer at right now?
-      Position & getPosition() { return position; }
+   // generate a new position for the howitzer
+   void generatePosition(const Position& posUpperRight)
+   {
+      double xPixels = random(posUpperRight.getPixelsX() * 0.1,
+         posUpperRight.getPixelsX() * 0.9);
+      position.setPixelsX(xPixels);
+      position.setPixelsY(0);
+   }
 
-      // generate a new position for the howitzer
-      void generatePosition(const Position& posUpperRight)
-      {
-         double xPixels = random(posUpperRight.getPixelsX() * 0.1,
-            posUpperRight.getPixelsX() * 0.9);
-         position.setPixelsX(xPixels);
-         position.setPixelsY(0);
-      }
+   // get the muzzle velocity
+   double getMuzzleVelocity() const { return muzzleVelocity; }
 
-      // get the muzzle velocity
-      double getMuzzleVelocity() const { return muzzleVelocity; }
+   // move the angle of the howitzer
+   void rotate(double radian) { elevation.add(radian); }
 
-      // move the angle of the howitzer
-      void rotate(double radian) { elevation.add(radian); }
+   // raise (or lower) the howitzer
+   void raise(double radian)
+   {
+      elevation.add(elevation.isRight() ? -radian : radian);
+   }
 
-      // raise (or lower) the howitzer
-      void raise(double radian)
-      {
-         elevation.add(elevation.isRight() ? -radian : radian);
-      }
+   // get the elevation
+   const Angle& getElevation() const { return elevation; }
+   void setElevation(double amtRadians) {
+      elevation.setRadians(amtRadians);
+   }
 
-      // get the elevation
-      const Angle & getElevation() const { return elevation; }
-
-      void setElevation(double radians)
-      {
-         elevation.setRadians(radians);
-      }
-
-   private:
-      Position position;      // initial position of the projectile
-      double muzzleVelocity;  // muzzle velocity, defaults to 827.0 m/s
-      Angle elevation;        // the elevation of the howitzer where 0 is up and positive is right.
+private:
+   Position position;      // initial position of the projectile
+   double muzzleVelocity;  // muzzle velocity, defaults to 827.0 m/s
+   Angle elevation;        // the elevation of the howitzer where 0 is up and positive is right.
 };
